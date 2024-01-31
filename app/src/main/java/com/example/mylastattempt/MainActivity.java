72f2ca131e307afc6afe3ce322bd09fcc1080581
prajwalcordiero <148431271+prajwalcordiero@users.noreply.github.com>
2024-01-31 14:15:36 +0530

    Final Commit

---
+++ app/src/main/java/com/example/mylastattempt/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mylastattempt;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        myMap = googleMap;
+
+        LatLng vamanjoor = new LatLng(12.91073,74.89867);
+        myMap.addMarker(new MarkerOptions().position(vamanjoor).title("Vamanjoor"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(vamanjoor));
+    }
+}
